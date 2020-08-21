8217dc631fed15413a13f21b406f4087737b96f8
Pete Hawkins <pete@alternatelabs.co>
2020-08-21 14:57:49 +0100

    feat(success url): redirect to success URL if set after successful payment

--- src/index.js
+++ src/index.js
@@ -1,98 +1,147 @@
-import styles from "./styles/embed.module.css"
-import elementClosest from "element-closest"
-import "element-remove"
-import "core-js/features/array/fill"
-import "core-js/features/array/fill"
-import "core-js/features/array/includes"
-import "core-js/features"
-import "core-js/features/promise"
+import styles from "./styles/embed.module.css";
+import elementClosest from "element-closest";
+import "element-remove";
+import "core-js/features/array/fill";
+import "core-js/features/array/fill";
+import "core-js/features/array/includes";
+import "core-js/features";
+import "core-js/features/promise";
 
 class PayHereClient {
   constructor() {
-    this.setupButtonListeners()
-    this.setupIframeListeners()
-    this.modalOpen = false
-    elementClosest(window)
+    this.setupButtonListeners();
+    this.setupIframeListeners();
+    this.modalOpen = false;
+    elementClosest(window);
   }
 
-  launch({embedURL, customerName, customerEmail, disableCustomer, amountInCents, hideAmount, customFields, onSuccess, onFailure, onClose}) {
-    PayHere.onSuccess = onSuccess || function(){}
-    PayHere.onFailure = onFailure || function(){}
-    PayHere.onClose = onClose || function(){}
-    var origin = window.location.origin
+  launch({
+    embedURL,
+    customerName,
+    customerEmail,
+    disableCustomer,
+    amountInCents,
+    hideAmount,
+    customFields,
+    onSuccess,
+    onFailure,
+    onClose,
+  }) {
+    PayHere.onSuccess = onSuccess || function () {};
+    PayHere.onFailure = onFailure || function () {};
+    PayHere.onClose = onClose || function () {};
+    var origin = window.location.origin;
 
-    var embedFrame = document.createElement("iframe")
-    var customFieldsStr = customFields ? btoa(JSON.stringify(customFields)) : ""
-    customerEmail = customerEmail ? encodeURIComponent(customerEmail) : ""
-    embedFrame.src = `${embedURL}?embedded=yes&embed_origin=${origin}&customer_name=${customerName || ""}&customer_email=${customerEmail || ""}&disable_customer_fields=${disableCustomer || "no"}&hide_amount=${hideAmount || "no"}&amount_in_cents=${amountInCents || ""}&custom_fields=${customFieldsStr}`
-    embedFrame.className = styles.embedIframe
+    var embedFrame = document.createElement("iframe");
+    var customFieldsStr = customFields
+      ? btoa(JSON.stringify(customFields))
+      : "";
+    customerEmail = customerEmail ? encodeURIComponent(customerEmail) : "";
+    embedFrame.src = `${embedURL}?embedded=yes&embed_origin=${origin}&customer_name=${
+      customerName || ""
+    }&customer_email=${customerEmail || ""}&disable_customer_fields=${
+      disableCustomer || "no"
+    }&hide_amount=${hideAmount || "no"}&amount_in_cents=${
+      amountInCents || ""
+    }&custom_fields=${customFieldsStr}`;
+    embedFrame.className = styles.embedIframe;
 
-    var embedWrap = document.createElement("div")
-    embedWrap.className = styles.embedModal
-    embedWrap.appendChild(embedFrame)
+    var embedWrap = document.createElement("div");
+    embedWrap.className = styles.embedModal;
+    embedWrap.appendChild(embedFrame);
 
-    var loadingSpinner = document.createElement("div")
-    loadingSpinner.className = styles.ldsRing
-    var div = document.createElement("div")
-    Array(4).fill(4).forEach(function() {
-      loadingSpinner.appendChild(div)
-    })
+    var loadingSpinner = document.createElement("div");
+    loadingSpinner.className = styles.ldsRing;
+    var div = document.createElement("div");
+    Array(4)
+      .fill(4)
+      .forEach(function () {
+        loadingSpinner.appendChild(div);
+      });
 
-    var embedContainer = document.createElement("div")
-    embedContainer.className = styles.embedContainer
-    embedContainer.appendChild(loadingSpinner)
-    embedContainer.appendChild(embedWrap)
+    var embedContainer = document.createElement("div");
+    embedContainer.className = styles.embedContainer;
+    embedContainer.appendChild(loadingSpinner);
+    embedContainer.appendChild(embedWrap);
 
-    document.body.appendChild(embedContainer)
+    document.body.appendChild(embedContainer);
   }
 
   setupButtonListeners() {
-    document.addEventListener("click", function(e) {
-      const match = e.target.closest("[data-payhere-embed]")
+    document.addEventListener(
+      "click",
+      function (e) {
+        const match = e.target.closest("[data-payhere-embed]");
 
-      if (!match || PayHere.modalOpen) return
-      e.preventDefault()
-      e.stopPropagation()
+        if (!match || PayHere.modalOpen) return;
+        e.preventDefault();
+        e.stopPropagation();
 
-      const embedURL = match.getAttribute("data-payhere-embed")
-      const customerName = match.getAttribute("data-payhere-customer-name")
-      const customerEmail = match.getAttribute("data-payhere-customer-email")
-      const disableCustomer = match.getAttribute("data-payhere-disable-customer")
-      const amountInCents = match.getAttribute("data-payhere-amount")
-      const hideAmount = match.getAttribute("data-payhere-hide-amount")
+        const embedURL = match.getAttribute("data-payhere-embed");
+        const customerName = match.getAttribute("data-payhere-customer-name");
+        const customerEmail = match.getAttribute("data-payhere-customer-email");
+        const disableCustomer = match.getAttribute(
+          "data-payhere-disable-customer"
+        );
+        const amountInCents = match.getAttribute("data-payhere-amount");
+        const hideAmount = match.getAttribute("data-payhere-hide-amount");
 
-      PayHere.modalOpen = true
+        PayHere.modalOpen = true;
 
-      PayHere.launch({embedURL, customerName, customerEmail, disableCustomer, amountInCents, hideAmount})
-    }, false)
+        PayHere.launch({
+          embedURL,
+          customerName,
+          customerEmail,
+          disableCustomer,
+          amountInCents,
+          hideAmount,
+        });
+      },
+      false
+    );
   }
 
   setupIframeListeners() {
-    window.addEventListener("message", function(e) {
-      switch (e.data.message) {
-        case "payhere:ready":
-          const loadingSpinner = document.querySelector("." + styles.ldsRing)
-          const embedWrap = document.querySelector("." + styles.embedModal)
-          loadingSpinner.remove()
-          embedWrap.style.transform = "scale(1)"
-          break;
-        case "payhere:success":
-          PayHere.onSuccess(e.data.content)
-          break;
-        case "payhere:failure":
-          PayHere.onFailure(e.data.content)
-          break;
-        case "payhere:close":
-          const embed = document.querySelector("." + styles.embedContainer)
-          embed && embed.remove()
-          PayHere.modalOpen = false
-          PayHere.onClose()
-          break;
-      }
-    }, false)
+    window.addEventListener(
+      "message",
+      function (e) {
+        switch (e.data.message) {
+          case "payhere:ready":
+            const loadingSpinner = document.querySelector("." + styles.ldsRing);
+            const embedWrap = document.querySelector("." + styles.embedModal);
+            loadingSpinner.remove();
+            embedWrap.style.transform = "scale(1)";
+            break;
+          case "payhere:success":
+            const data = e.data.content;
+            PayHere.onSuccess(data);
+            if (
+              data.plan &&
+              data.plan.success_url &&
+              data.plan.success_url.length > 0
+            ) {
+              console.log("Redirecting to " + data.plan.success_url);
+              setTimeout(() => {
+                window.location.href = data.plan.success_url;
+              }, 2000);
+            }
+            break;
+          case "payhere:failure":
+            PayHere.onFailure(e.data.content);
+            break;
+          case "payhere:close":
+            const embed = document.querySelector("." + styles.embedContainer);
+            embed && embed.remove();
+            PayHere.modalOpen = false;
+            PayHere.onClose();
+            break;
+        }
+      },
+      false
+    );
   }
 }
 
 if (typeof window.PayHere === "undefined") {
-  window.PayHere = new PayHereClient
+  window.PayHere = new PayHereClient();
 }
--- src/react/embed.jsx
+++ src/react/embed.jsx
@@ -1,63 +1,99 @@
-import React, { useEffect, useCallback, useState } from "react"
-import ReactDOM from "react-dom"
-import styles from "../styles/embed.module.css"
+import React, { useEffect, useCallback, useState } from "react";
+import ReactDOM from "react-dom";
+import styles from "../styles/embed.module.css";
 
-const PayhereEmbed = ({ open, selector, embedURL, customerName, customerEmail, disableCustomer, amountInCents, hideAmount, customFields, onSuccess, onFailure, onClose }) => {
-  const [isLoading, setLoading] = useState(true)
+const PayhereEmbed = ({
+  open,
+  selector,
+  embedURL,
+  customerName,
+  customerEmail,
+  disableCustomer,
+  amountInCents,
+  hideAmount,
+  customFields,
+  onSuccess,
+  onFailure,
+  onClose,
+}) => {
+  const [isLoading, setLoading] = useState(true);
 
   // Don't load in non browser context
-  if (typeof window === "undefined") return null
+  if (typeof window === "undefined") return null;
 
-  const origin = window.location.origin
-  const customFieldsStr = customFields ? btoa(JSON.stringify(customFields)) : ""
-  customerEmail = customerEmail ? encodeURIComponent(customerEmail) : ""
+  const origin = window.location.origin;
+  const customFieldsStr = customFields
+    ? btoa(JSON.stringify(customFields))
+    : "";
+  customerEmail = customerEmail ? encodeURIComponent(customerEmail) : "";
 
-  const onMessage = useCallback(e => {
-    switch (e.data.message) {
-      case "payhere:ready":
-        setLoading(false)
-        break;
-      case "payhere:success":
-        onSuccess(e.data.content)
-        break;
-      case "payhere:failure":
-        onFailure(e.data.content)
-        break;
-      case "payhere:close":
-        onClose()
-        setLoading(true)
-        break;
-    }
-  }, [onSuccess, onFailure, onClose])
+  const onMessage = useCallback(
+    (e) => {
+      switch (e.data.message) {
+        case "payhere:ready":
+          setLoading(false);
+          break;
+        case "payhere:success":
+          const data = e.data.content;
+          onSuccess(data);
+          if (
+            data.plan &&
+            data.plan.success_url &&
+            data.plan.success_url.length > 0
+          ) {
+            console.log("Redirecting to " + data.plan.success_url);
+            setTimeout(() => {
+              window.location.href = data.plan.success_url;
+            }, 2000);
+          }
+          break;
+        case "payhere:failure":
+          onFailure(e.data.content);
+          break;
+        case "payhere:close":
+          onClose();
+          setLoading(true);
+          break;
+      }
+    },
+    [onSuccess, onFailure, onClose]
+  );
 
   useEffect(() => {
-    window.addEventListener("message", onMessage, false)
-    return () => window.removeEventListener("message", onMessage, false)
-  })
+    window.addEventListener("message", onMessage, false);
+    return () => window.removeEventListener("message", onMessage, false);
+  });
 
-  if (!open) return null
+  if (!open) return null;
 
   return ReactDOM.createPortal(
-    (
-      <div className={styles.embedContainer}>
-        {isLoading &&
-          <div className={styles.ldsRing}>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        }
-        <div className={styles.embedModal} style={{ transform: isLoading ? `scale(0)` : `scale(1)` }}>
-          <iframe
-            className={styles.embedIframe}
-            src={`${embedURL}?embedded=yes&embed_origin=${origin}&customer_name=${customerName || ""}&customer_email=${customerEmail || ""}&disable_customer_fields=${disableCustomer || "no"}&hide_amount=${hideAmount || "no"}&amount_in_cents=${amountInCents || ""}&custom_fields=${customFieldsStr}`}
-          ></iframe>
+    <div className={styles.embedContainer}>
+      {isLoading && (
+        <div className={styles.ldsRing}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
+      )}
+      <div
+        className={styles.embedModal}
+        style={{ transform: isLoading ? `scale(0)` : `scale(1)` }}
+      >
+        <iframe
+          className={styles.embedIframe}
+          src={`${embedURL}?embedded=yes&embed_origin=${origin}&customer_name=${
+            customerName || ""
+          }&customer_email=${customerEmail || ""}&disable_customer_fields=${
+            disableCustomer || "no"
+          }&hide_amount=${hideAmount || "no"}&amount_in_cents=${
+            amountInCents || ""
+          }&custom_fields=${customFieldsStr}`}
+        ></iframe>
       </div>
-    ),
+    </div>,
     document.querySelector(selector)
-  )
-}
+  );
+};
 
-export default PayhereEmbed
+export default PayhereEmbed;
